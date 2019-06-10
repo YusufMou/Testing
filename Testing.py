d735d0018281671b83488364613f408bdb89c9a1
YusufMou <47399245+YusufMou@users.noreply.github.com>
2019-06-10 14:28:49 -0400

    updated code

--- Testing.py
+++ Testing.py
@@ -1,17 +1,27 @@
 from tkinter import *
 import time
+#Create window
 master = Tk()
 
+#Create canvas
 w = Canvas(master, width=900, height=600, bg="blue")
 w.pack()
 
-rectangle = w.create_rectangle(50, 20, 150, 80, fill="red")
-y = x = 5
+#Create ball
+shape = w.create_oval(0, 0, 20, 20, fill='red')
+x = y = 5
 
+#Moving the ball
+for ball in range(0 ,100):
+  w.move(shape, x, y)
+  master.update()
+  time.sleep(.01)
 
-for loops in range(500):
-    time.sleep(0.025)
-    w.move(rectangle, x, y)
-    w.update()
 master.mainloop()
-
+# class ball(self):
+# 	self.shape = w.create_oval(0, 0, 50, 50, fill='red')
+# pos = w.coords(shape)
+# if pos[2] >= WIDTH or pos[0] <= 0:
+#     speedx *= -1
+# if pos[3] >= HEIGHT or pos[1] <= 0:
+#     speedy *= -1
