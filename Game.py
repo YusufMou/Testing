37b09b1ce5ec91c034a71b130c3e9a98f0381a4b
YusufMou <47399245+YusufMou@users.noreply.github.com>
2019-06-12 20:50:58 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -11,20 +11,21 @@ class Ball:
         self.canvas.move(self.id, 245, 100)
         self.xspeed = random.randrange(-3,3)
         self.yspeed = -1
-        self.hit_bottom = False
+        self.hit_right = False
         self.score = 0
 
     def draw(self):
         self.canvas.move(self.id, self.xspeed, self.yspeed)
         pos = self.canvas.coords(self.id)
+        print (str(pos))
         if pos[1] <= 0:
             self.yspeed = 3
-        if pos[3] >= 400:
-            self.hit_bottom = True
+        if pos[3] >= 500:
+            self.yspeed = -3
         if pos[0] <= 0:
             self.xspeed = 3
-        if pos[2] >= 500:
-            self.xspeed = -3
+        if pos[2] >= 900:
+            self.hit_right = True
         if self.hit_paddle(pos) == True:
             self.yspeed = -3
             self.xspeed = random.randrange(-3,3)
@@ -42,45 +43,71 @@ class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0,100,10,0, fill=color)
-        self.canvas.move(self.id, 200, 300)
+        self.canvas.move(self.id, 850, 200)
         self.yspeed = 0
         self.canvas.bind_all('<KeyPress-Up>', self.move_up)
         self.canvas.bind_all('<KeyPress-Down>', self.move_down)
 
     def draw(self):
-        self.canvas.move(self.id, self.yspeed, 0)
+        self.canvas.move(self.id, 0, self.yspeed)
         pos = self.canvas.coords(self.id)
+        # print (str(pos))
         if pos[1] <= 0:
             self.yspeed = 0
         if pos[3] >= 500:
             self.yspeed = 0
 
     def move_down(self, evt):
-        self.yspeed = -2
-    def move_up(self, evt):
         self.yspeed = 2
+    def move_up(self, evt):
+        self.yspeed = -2
+
+class Paddle_b:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id_b = create_rectangle(0, 100, 10, 0, fill=color)
+        self.canvas.move(self.id_b, 50, 200)
+        self.yspeed = 0
+        self.canvas.bind_all('<KeyPress-w>', self.move_up)
+        self.canvas.bind_all('<KeyPress-s>', self.move_down)
+
+    def draw(self):
+        self.convas.move(self.id_b, 0, self.yspeed)
+        pos = self.canvas.coords(self.id_b)
+        if pos[1] <= 0:
+            self.yspeed = 0
+        if pos[3] >= 500:
+            self.yspeed = 0
+
+    def move_down(self, evt):
+        self.yspeed = 2
+    def move_up(self, evt):
+        self.yspeed = -2
+
 
 # Create window and canvas to draw on
 tk = Tk()
 tk.title("Ball Game")
-canvas = Canvas(tk, width=500, height=400, bd=0, bg='papaya whip')
+canvas = Canvas(tk, width=900, height=500, bd=0, bg='papaya whip')
 canvas.pack()
 label = canvas.create_text(5, 5, anchor=NW, text="Score: 0")
 tk.update()
 paddle = Paddle(canvas, 'blue')
-ball = Ball(canvas, 'red', 25, paddle)
+paddle_b = Paddle_b(canvas, 'red')
+ball = Ball(canvas, 'red', 25, paddle, paddle_b)
 
 # Animation loop
-while ball.hit_bottom == False:
+while ball.hit_right == False:
     ball.draw()
     paddle.draw()
+    paddle_b.draw()
     canvas.itemconfig(label, text="Score: "+str(ball.score))
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
 
 # Game Over
-go_label = canvas.create_text(250,200,text="GAME OVER",font=("Helvetica",30))
+go_label = canvas.create_text(450,250,text="GAME OVER",font=("Helvetica",30))
 tk.update()
 
 tk.mainloop()
